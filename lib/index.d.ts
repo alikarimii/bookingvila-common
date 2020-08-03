09e4d559a7c6eaa32ec2ade279f29b3279a46fc2
bookingvila <bookingvila@bookingvila.local>
2020-08-03 13:29:10 +0430

    Updates

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,7 @@
 export * from "./error/database-connection";
 export * from "./error/bad-request";
+export * from "./error/forbidden";
+export * from "./error/notAuthorize";
 export * from "./middleware/error-handler";
 export * from "./middleware/validation-factory";
 export * from "./util/logger";
