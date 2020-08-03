49dfb6f71ceeb09a4982b6ffc6bc7f1182fda917
bookingvila <bookingvila@bookingvila.local>
2020-08-03 13:23:41 +0430

    add 2 error

---
+++ src/error/forbidden.ts
@@ -0,0 +1,13 @@
+import { CustomError } from "./custom-error";
+
+export class ForbiddenError extends CustomError {
+  statusCode = 403;
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, ForbiddenError.prototype);
+  }
+
+  serializeError() {
+    return [{ message: this.message }];
+  }
+}
