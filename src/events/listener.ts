3b5ab6a2b6055043426cbf2b463656dcd6c39b33
bookingvila <bookingvila@bookingvila.local>
2020-08-08 13:23:40 +0430

    add publisher and listener

---
+++ src/events/listener.ts
@@ -0,0 +1,45 @@
+import { ConsumeMessage, Channel } from 'amqplib'
+import { logger } from '../util/logger';
+
+export abstract class Listener<TData> {
+    abstract queueName: string;
+    abstract onMessage(data: TData, msg: ConsumeMessage): void;
+    abstract prefetch: number
+    abstract noAck: boolean = false
+
+    constructor(protected channel: Channel) {
+    }
+    listen() {
+        this.subscriptionOption()
+        this.channel.consume(this.queueName, (msg: ConsumeMessage | null) => {
+            if (!msg) {
+                return;
+            }
+            try {
+                let data: TData = JSON.parse(msg.content.toString());
+                this.onMessage(data, msg)
+            } catch (error) {
+                logger.error({ message: JSON.stringify(error), label: "Listener:listen" })
+            }
+        }, { noAck: !!this.noAck })
+    }
+    private subscriptionOption() {
+        try {
+            // no exchange => rond robin
+            this.channel
+                .assertQueue(this.queueName, {
+                    durable: true
+                })
+            console.log(
+                " [*] Waiting for messages in %s. To exit press CTRL+C",
+                this.queueName
+            );
+            if (this.prefetch) this.channel.prefetch(this.prefetch);
+        } catch (error) {
+            logger.error({ message: JSON.stringify(error), label: "Listener:subscriptionOption" })
+            return
+        }
+    }
+
+
+}
---
+++ src/events/publisher.ts
@@ -0,0 +1,46 @@
+import { Channel } from "amqplib";
+import { logger } from "../util/logger";
+
+export abstract class Publisher<TData> {
+    abstract queueName: string
+    constructor(protected channel: Channel) { }
+
+    async publish(data: TData): Promise<void> {
+        try {
+            // no exchange => rond robin
+            await this.channel.assertQueue(this.queueName, {
+                durable: true
+            })
+            await this.channel.sendToQueue(this.queueName, Buffer.from(JSON.stringify(data)), {
+                persistent: true,
+            })
+        } catch (error) {
+            logger.error({ message: JSON.stringify(error), label: "Publisher:publish" })
+        }
+    }
+    // must install rabbitmq_delayed_message_exchange-3.8.0 plugin in rabbitmq
+    async publishWithDelay(data: TData, delay: number): Promise<void> {
+        try {
+            await this.channel.assertExchange("my-exchange", "x-delayed-message", {
+                durable: true,
+                arguments: { "x-delayed-type": "direct" },
+            });
+            await this.channel.bindQueue(this.queueName, "my-exchange", this.queueName);
+            // Publish message with a delay of ? ms
+            const headers = { "x-delay": delay };
+            this.channel.publish(
+                "my-exchange",
+                this.queueName,
+                Buffer.from(JSON.stringify(data)),
+                {
+                    headers,
+                    persistent: true,
+                }
+            );
+
+        } catch (error) {
+            logger.error({ message: JSON.stringify(error), label: "Publisher:publishWithDelay" })
+        }
+    }
+}
+
