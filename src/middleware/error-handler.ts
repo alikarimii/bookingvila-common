1ab33a68219e3c855872cc2fe51664c31693c045
bookingvila <bookingvila@bookingvila.local>
2020-08-09 09:12:34 +0430

    fix errorHandler middleware

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { CustomError } from "../error/custom-error";
+import { logger } from "../util/logger";
 export const errorHandler = (
   err: Error,
   req: Request,
@@ -9,6 +10,7 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({ errors: err.serializeError() });
   }
+  logger.error({ message: JSON.stringify(err), label: "errorHandler" })
   res.status(400).json({
     errors: [{ message: "Something went Wrong" }],
   });
